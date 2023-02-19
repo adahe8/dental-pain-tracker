112461a4afb191eefccf5519589e104982020e7e
adahe8 <ada.he0908@gmail.com>
2023-02-18 16:48:41 -0800

    linking session & end pages, cleaning up Timer

--- project-software/pages/session.js
+++ project-software/pages/session.js
@@ -2,13 +2,18 @@
 //general swathes of pain area lit up on screen
 // page to be displayed on dentist/ provider's screen during dentist session
 import Timer from '../components/Timer'
+import { useRouter } from 'next/router'
 
 export default function session(){
+    const router = useRouter();
+    const handleClick = () => {
+        
+    }
     return (
         <div> 
             <h2> Patient Updates </h2>
             <Timer />
-            
+            <button className="endSession" onClick={() => router.push('./endsummary')}> End Session </button>
         </div>
     )
 }
