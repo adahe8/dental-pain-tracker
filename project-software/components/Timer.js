95ca98fd81381aec43dcb958c4045ecd4c19fd73
adahe8 <ada.he0908@gmail.com>
2023-02-18 16:49:13 -0800

    timer updates

--- project-software/components/Timer.js
+++ project-software/components/Timer.js
@@ -28,9 +28,15 @@ function Timer() {
                 </div>
             </div>
             <div>
-                <button onClick={() => setTimerOn(false)}> Stop </button>
-                <button onClick={() => setTimerOn(true)}> Resume </button>
-                <button onClick={() => setTime(0)}> Reset </button>
+                {timerOn && (
+                    <button onClick={() => setTimerOn(false)}> Stop </button>
+                )}
+                {!timerOn && time!==0 && (
+                    <button onClick={() => setTimerOn(true)}> Resume </button>
+                )}
+                {!timerOn && time > 0 && (
+                    <button onClick={() => setTime(0)}> Reset </button>
+                )}
             </div>
         </div>
     )
---
+++ project-software/pages/endsummary.js
@@ -0,0 +1,7 @@
+
+export default function endsummary(){
+    return(
+        <div>
+        </div>
+    )
+}
