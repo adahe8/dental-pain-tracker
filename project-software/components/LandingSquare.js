1eaa31a65169dda1a7b3401e5902c87874e51a56
adahe8 <ada.he0908@gmail.com>
2023-02-18 17:31:55 -0800

    edited app description

--- project-software/components/LandingSquare.js
+++ project-software/components/LandingSquare.js
@@ -2,8 +2,8 @@
 const LandingSquare = ({ router }) => {
     return(
         <div>
-            <h1>Landing</h1>
-            <p> App description here </p>
+            <h1>PMT</h1>
+            <p> A monitor for your patient's oral comfort, to ensure your dental practice puts them first. </p>
             <button className="go" onClick={() => router.push('../session')}> Start Session </button>
         </div>
     )
