522354c4ce5ca6f96478725bb68091a0fcc90223
adahe8 <ada.he0908@gmail.com>
2023-02-18 17:41:32 -0800

    details for end summary

--- project-software/pages/endsummary.js
+++ project-software/pages/endsummary.js
@@ -2,6 +2,10 @@
 export default function endsummary(){
     return(
         <div>
+            <h2>Session Summary</h2>
+            <h3>Duration: </h3>
+            <h3>Instances of pain: </h3>
+            <h3>Average pain rating: </h3>
         </div>
     )
 }
